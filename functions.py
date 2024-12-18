db3b741eeea8c11721cd7ccbe64cc52ee09cdb07
wilbertr <125577618+wilbertr@users.noreply.github.com>
2024-12-18 17:18:53 +0100

    add noise to measurement

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_formation(z):
+def plot_formation(z, goal=None):
     """
     This function plots the formation in graph representation for a formation control project.
     
@@ -20,76 +20,49 @@ def plot_formation(z):
         [0, 0, 0, 0, 1, -1, 0, 0, -1, 0, 0, 0],
         [0, 0, 0, 1, -1, 0, 0, 1, 0, 0, 0, 0]
     ])
-        # Number of nodes (N) and edges (M)
     N, M = B.shape
 
-    # Step 1: Find the edge set using np.nonzero
-    row_indices, col_indices = np.nonzero(B)  # Get row and column indices where B is non-zero
+    z_star = np.array([[ 2,  0],
+                        [ 1,  1],
+                        [ 1, -1],
+                        [ 0,  1],
+                        [ 0, -1],
+                        [-1,  1],
+                        [-1, -1]])
     
-    # Pair up the indices to create edges
-    edges = []
-    for edge_id in range(M):
-        # Find where the column index matches the edge_id
-        rows_for_edge = row_indices[col_indices == edge_id]
-        if len(rows_for_edge) == 2:  # Each edge should have exactly two nodes connected
-            edges.append((rows_for_edge[0], rows_for_edge[1]))
-    
-    # Step 2: Plot the formation
-    plt.figure()
-    plt.gca().set_aspect('equal')  # Set aspect ratio to make the plot square
-    plt.axis([-2, 2, -2, 2])       # Set axis limits
-    
-    # Plot edges (connections between nodes)
-    for edge in edges:
-        node1, node2 = edge
-        x_values = [z[node1, 0], z[node2, 0]]  # X-coordinates of the two nodes
-        y_values = [z[node1, 1], z[node2, 1]]  # Y-coordinates of the two nodes
-        plt.plot(x_values, y_values, 'k-', linewidth=1.5)  # Black lines for edges
-    
-    # Plot nodes (agents)
-    for i in range(N):
-        plt.plot(z[i, 0], z[i, 1], 'ro', markersize=10)  # Red dots for nodes
-    
-    # Finalize plot
-    plt.title("Formation Plot")
-    plt.xlabel("X-axis")
-    plt.ylabel("Y-axis")
-    plt.grid(True)
-    plt.show()
+    D = z_star.shape[1]
+    if len(z.shape) == 3:
+        z_end = z[:, :, -1]
+        z_star = z_star.reshape(N, D, 1)
+        plot_trajectory = True
+    else:
+        z_end = z
+        plot_trajectory = False
 
-    # # Number of nodes (N) and edges (M)
-    # N, M = B.shape
 
-    # # Step 1: Find the edge set using np.nonzero
-    # row_indices, col_indices = np.nonzero(B)  # Get row and column indices where B is non-zero
-    
-    # # Pair up the indices to create edges
-    # edges = []
-    # for edge_id in range(M):
-    #     # Find where the column index matches the edge_id
-    #     rows_for_edge = row_indices[col_indices == edge_id]
-    #     if len(rows_for_edge) == 2:  # Each edge should have exactly two nodes connected
-    #         edges.append((rows_for_edge[0], rows_for_edge[1]))
-    
-    # # Step 2: Plot the formation
-    # plt.figure()
-    # plt.gca().set_aspect('equal')  # Set aspect ratio to make the plot square
-    # plt.axis([-2, 2, -2, 2])       # Set axis limits
-    
-    # # Plot edges (connections between nodes)
-    # for edge in edges:
-    #     node1, node2 = edge
-    #     x_values = [z[node1, 0], z[node2, 0]]  # X-coordinates of the two nodes
-    #     y_values = [z[node1, 1], z[node2, 1]]  # Y-coordinates of the two nodes
-    #     plt.plot(x_values, y_values, 'k-', linewidth=1.5)  # Black lines for edges
-    
-    # # Plot nodes (agents)
-    # for i in range(N):
-    #     plt.plot(z[i, 0], z[i, 1], 'ro', markersize=10)  # Red dots for nodes
-    
-    # # Finalize plot
-    # plt.title("Formation Plot")
-    # plt.xlabel("X-axis")
-    # plt.ylabel("Y-axis")
-    # plt.grid(True)
-    # plt.show()
+    fig, axes = plt.subplot_mosaic('ab', figsize=(8,4), constrained_layout=True)
+    ax = axes['a']
+    for i in range(M):
+        ax.plot(z_end[B[:, i]!=0, 0],z_end[B[:, i]!=0, 1], c='k', linewidth=1, zorder=0)
+    for i in range(3,N):
+        ax.scatter(z_end[i,0], z_end[i,1], color='tab:green', s=50, zorder=2)
+    for i in range(0,3):
+        ax.scatter(z_end[i,0], z_end[i,1], color='tab:orange', s=50, zorder=2)
+    ax.set_aspect('equal')
+    ax.set_xlim([-2, 2])
+    ax.set_ylim([-2, 2])
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('Formation')
+
+    if plot_trajectory:
+        ax.plot(z[:, 0, :].T, z[:, 1, :].T, c='limegreen', linewidth=2, zorder=1)
+
+    error = np.sum(np.linalg.norm(z-z_star, axis=1), axis=0)
+    ax = axes['b']
+    # ax.set_aspect('equal')
+    ax.semilogy(error)
+    ax.set_xlabel('# timesteps')
+    ax.set_ylabel('Error')
+    ax.set_title('Error in the formation')
