2c3b074387a26de304d149ed1824ff95dc850fc8
wilbertr <125577618+wilbertr@users.noreply.github.com>
2024-12-20 17:29:25 +0100

    made plots nice and added the dependence on T

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.style.use('matplotlibrc')
+import matplotlibcolors
 
 
-def plot_formation(z, goal=None):
+def plot_formation(z, ax, title='Formation'):
     """
     This function plots the formation in graph representation for a formation control project.
     
@@ -21,49 +23,36 @@ def plot_formation(z, goal=None):
         [0, 0, 0, 1, -1, 0, 0, 1, 0, 0, 0, 0]
     ])
     N, M = B.shape
-
-    z_star = np.array([[ 2,  0],
-                        [ 1,  1],
-                        [ 1, -1],
-                        [ 0,  1],
-                        [ 0, -1],
-                        [-1,  1],
-                        [-1, -1]])
     
-    D = z_star.shape[1]
+    D = z.shape[1]
     if len(z.shape) == 3:
         z_end = z[:, :, -1]
-        z_star = z_star.reshape(N, D, 1)
         plot_trajectory = True
     else:
         z_end = z
         plot_trajectory = False
 
+    colors = ['b', 'o', 'g', 'y', 'p', 'lb', 'r']
 
-    fig, axes = plt.subplot_mosaic('ab', figsize=(8,4), constrained_layout=True)
-    ax = axes['a']
     for i in range(M):
-        ax.plot(z_end[B[:, i]!=0, 0],z_end[B[:, i]!=0, 1], c='k', linewidth=1, zorder=0)
+        ax.plot(z_end[B[:, i]!=0, 0],z_end[B[:, i]!=0, 1], c='k', linewidth=.5, zorder=0)
     for i in range(3,N):
-        ax.scatter(z_end[i,0], z_end[i,1], color='tab:green', s=50, zorder=2)
+        ax.scatter(z_end[i,0], z_end[i,1], color=colors[i-2], edgecolor='k', s=50, zorder=2, marker='o')
     for i in range(0,3):
-        ax.scatter(z_end[i,0], z_end[i,1], color='tab:orange', s=50, zorder=2)
-    ax.set_aspect('equal')
-    ax.set_xlim([-2, 2])
-    ax.set_ylim([-2, 2])
-    ax.grid()
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_title('Formation')
+        ax.scatter(z_end[i,0], z_end[i,1], color=colors[0], edgecolor='k', s=50, zorder=2, marker='o')
 
-    if plot_trajectory:
-        ax.plot(z[:, 0, :].T, z[:, 1, :].T, c='limegreen', linewidth=2, zorder=1)
+    if plot_trajectory:    
+        for i in range(3,N):
+            ax.plot(z[i, 0, :], z[i, 1, :], color=colors[i-2], linewidth=1, zorder=1)
 
-    error = np.sum(np.linalg.norm(z-z_star, axis=1), axis=0)
-    ax = axes['b']
+
+def plot_error(z_star, *z, labels):
+    fig, axes = plt.subplot_mosaic('a', figsize=(4,4), constrained_layout=True)
+    ax = axes['a']
+    for i, x in enumerate(z):
+        error = np.sum(np.linalg.norm(z-z_star, axis=1), axis=0)
+        ax.semilogy(error, labels[i])
     # ax.set_aspect('equal')
-    ax.semilogy(error)
     ax.set_xlabel('# timesteps')
     ax.set_ylabel('Error')
     ax.set_title('Error in the formation')
-    return fig
